5d30632a16ee516de4e795af086b4f207ab45f08
Nader <nader.kamel.the.programmer@gmail.com>
2022-03-28 17:18:19 +0200

    default middlewares

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,30 @@
 // بسم الله الرحمن الرحيم
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 const port = 3000;
 const app = express();
 const address = "0.0.0.0:" + port;
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 15 minutes
+    max: 30, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    message: "too many requests",
+});
+app.use(limiter);
+app.use(express.json());
+app.use(morgan("common"));
+app.use(helmet());
 app.get("/", (req, res) => {
-    res.send("Allah");
+    res.send("بسم الله الرحمن الرحيم");
+});
+app.post("/", (req, res) => {
+    res.json({
+        msg: "Allah",
+        data: req.body,
+    });
 });
 app.listen(port, () => {
     console.log(`server is live @${address}`);
